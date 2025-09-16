56ea22fa10f4458de04487b1506f589eca26c30e
shashi72ranjan <shashi72ranjan@gmail.com>
2025-09-16 16:08:09 +0530

    bugg

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: {
-    // ✅ Ignore ESLint errors during production builds
-    ignoreDuringBuilds: true,
-  },
+  /* config options here */
 };
 
 export default nextConfig;
